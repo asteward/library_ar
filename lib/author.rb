8f68b5df2a5c7c2219d6a22235cc2bbb1cfffa64
Austin Steward <austinsteward@aol.com>
2014-08-17 19:53:28 +0000

    Properly capitalize author name

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,13 @@
 class Author < ActiveRecord::Base
   validates :first_name, presence: true, length: { minimum: 1 }
   validates :last_name, presence: true, length: { minimum: 1 }
+  before_save :capitalize_name
   has_many :books
+
+
+private
+  def capitalize_name
+    first_name.capitalize!
+    last_name.capitalize!
+  end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -8,5 +8,9 @@ describe 'Author' do
     expect(Author.all).to eq [author]
   end
 
-
+  it 'properly capitalizes the first and last name of an author before saving' do
+    author = Author.create({first_name: "georgE", last_name: "orWeLl"})
+    expect(author.first_name).to eq "George"
+    expect(author.last_name).to eq "Orwell"
+  end
 end
