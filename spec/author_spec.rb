b98c4213239e33f6e1f2331502f3a727d8555b7e
Austin Steward <austinsteward@aol.com>
2014-08-17 19:38:49 +0000

    Update author_spec mistake

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -2,8 +2,11 @@ require 'spec_helper'
 
 describe 'Author' do
   it 'validates the presence of a first and last name for the author' do
+    author = Author.create({first_name: "", last_name: ""})
     expect(Author.all).to eq []
     author = Author.create({first_name: "George", last_name: "Orwell"})
     expect(Author.all).to eq [author]
   end
+
+
 end
