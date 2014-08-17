7a5d91826b8f9612a8e8d004d9a8602f4ec7fa59
Austin Steward <austinsteward@aol.com>
2014-08-17 21:58:18 +0000

    Add main menu to UI

--- library_ui.rb
+++ library_ui.rb
@@ -13,3 +13,27 @@ def header
  █▄▄▄█  ▀▀▀ █▀▀▀ ▀▀▀ ▀ ▀▀ 　  █▄▄█ ▀▀▀ ▀▀▀  ▀ ▀▀ ▀  ▀ ▀ ▀▀ ▄▄▄█ 　  █▄▄▄█ ▀▀▀ ▀  ▀ ▀ ▀▀ ▀▀▀ ▀  ▀
  \n\n"
 end
+
+def main_menu
+  header
+  puts "A > Author Menu"
+  puts "B > Book Menu" if (Author.all.length > 0)
+  puts "E > Exit\n"
+  print "Please select a menu option: "
+  choice = gets.chomp.upcase
+  (choice = "z") if ((Author.all.length == 0) && (choice == "B"))
+  case (choice)
+  when 'A'
+    author_menu
+  when 'B'
+    book_menu
+  when 'E'
+    puts "Have an excellent day!"
+    exit
+  else
+    puts "Invalid menu option. Try again."
+    sleep 1.5
+  end
+  main_menu
+end
+main_menu
